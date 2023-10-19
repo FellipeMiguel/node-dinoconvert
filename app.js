28d5e5120ec833225b99bac0c33a70bc68eb8a59
FellipeMiguel <fellipemiguel1@icloud.com>
2023-10-19 17:49:18 -0300

    Representando tabela em forma de classe

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const Reader = require('./Reader');
 const Processor = require('./Processor');
+const Table = require('./Table');
 
 const reader = new Reader();
 
 async function main() {
     const data = await reader.Read('./users.csv');
+    const processedData = Processor.Process(data);
 
-    Processor.Process(data);
+    const users = new Table(processedData);
+
+    console.log(users.rows);
+
 }
 
 main()
